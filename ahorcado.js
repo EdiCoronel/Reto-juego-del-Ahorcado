d521a7ab9e438ac2a8829823efe148f19a139e6e
Ediberto <coronelvediberto@gmail.com>
2022-02-15 20:37:07 -0300

    mejora de codigo dibujar

--- ahorcado.js
+++ ahorcado.js
@@ -74,7 +74,7 @@ function verificar(){
   dibujarMuñeco(intentos);
 }
 
-var listaPalabras = ['FELICIDAD','ALEGRIA','TRISTEZA','DOLOR','RABIA','ALERGIA'];
+var listaPalabras = ['CIENCIA','ALEGRIA','FIESTA','DOLOR','RABIA','ALERGIA'];
 
 // Cuando no se coloca una palabra se elige una al azar de la libreria
 
@@ -126,73 +126,78 @@ function validar1(e) {
 //Dibujar el ahorcado al fallar en la comparacion de las letras
 
 function dibujarMuñeco(intentos){
-  switch (intentos){
-    case 6:
-      const canva6 = document.getElementById("ahorcado");
-      const ctx6 = canva6.getContext("2d");
-      ctx6.lineWidth = 3;
-      ctx6.strokeStyle = "#000";
-      ctx6.beginPath();
-      ctx6.arc(200,40,20,0,Math.PI*2,true); 
-      ctx6.stroke();
-      break;
+  const muñeco = intentos
+  const dibujoAhorcado = {
 
-    case 5:
-      const canva5 = document.getElementById("ahorcado");
-      const ctx5 = canva5.getContext("2d");
-      ctx5.lineWidth = 3;
-      ctx5.beginPath();
-      ctx5.moveTo(200, 60);
-      ctx5.lineTo(200, 110);
-      ctx5.stroke();
-      break;
-
-    case 4:
-      const canva4 = document.getElementById("ahorcado");
-      const ctx4 = canva4.getContext("2d");
-      ctx4.lineWidth = 3;
-      ctx4.beginPath();
-      ctx4.moveTo(200, 65);
-      ctx4.lineTo(230, 90);
-      ctx4.stroke();
-      break;
-
-    case 3:
-      const canva3 = document.getElementById("ahorcado");
-      const ctx3 = canva3.getContext("2d");
-      ctx3.lineWidth = 3;
-      ctx3.beginPath();
-      ctx3.moveTo(200, 65);
-      ctx3.lineTo(170, 90);
-      ctx3.stroke();
-      break;
-
-    case 2:
-      const canva2 = document.getElementById("ahorcado");
-      const ctx2 = canva2.getContext("2d");
-      ctx2.lineWidth = 3;
-      ctx2.beginPath()
-      ctx2.moveTo(200, 110);
-      ctx2.lineTo(170, 140);
-      ctx2.stroke();
-      break;
-
-    case 1:
-      const canva1 = document.getElementById("ahorcado");
-      const ctx1 = canva1.getContext("2d");
-      ctx1.lineWidth = 3;
-      ctx1.beginPath()
-      ctx1.moveTo(200, 110);
-      ctx1.lineTo(230, 140);
-      ctx1.stroke();
-
-      for(i=0;i<numPalabras;i++){
-          document.getElementById('d'+i).value=arrayPalabras[i];
+      6 : function cabeza() {
+          const canva6 = document.getElementById("ahorcado");
+          const ctx6 = canva6.getContext("2d");
+          ctx6.lineWidth = 3;
+          ctx6.strokeStyle = "#000";
+          ctx6.beginPath();
+          ctx6.arc(200,40,20,0,Math.PI*2,true); 
+          ctx6.stroke();
+      },
+  
+      5 : function cuerpo() {
+          const canva5 = document.getElementById("ahorcado");
+          const ctx5 = canva5.getContext("2d");
+          ctx5.lineWidth = 3;
+          ctx5.beginPath();
+          ctx5.moveTo(200, 60);
+          ctx5.lineTo(200, 110);
+          ctx5.stroke();
+      },
+          
+          
+      4 : function brazoDerecho() {
+          const canva4 = document.getElementById("ahorcado");
+          const ctx4 = canva4.getContext("2d");
+          ctx4.lineWidth = 3;
+          ctx4.beginPath();
+          ctx4.moveTo(200, 65);
+          ctx4.lineTo(230, 90);
+          ctx4.stroke();
+      },
+          
+      3 : function brazoIzquierdo() {
+          const canva3 = document.getElementById("ahorcado");
+          const ctx3 = canva3.getContext("2d");
+          ctx3.lineWidth = 3;
+          ctx3.beginPath();
+          ctx3.moveTo(200, 65);
+          ctx3.lineTo(170, 90);
+          ctx3.stroke();
+      },
+          
+      2 : function piernaDerecha() {
+          const canva2 = document.getElementById("ahorcado");
+          const ctx2 = canva2.getContext("2d");
+          ctx2.lineWidth = 3;
+          ctx2.beginPath()
+          ctx2.moveTo(200, 110);
+          ctx2.lineTo(170, 140);
+          ctx2.stroke();
+      },
+      
+      1 : function piernaIzquierda() {
+          const canva1 = document.getElementById("ahorcado");
+          const ctx1 = canva1.getContext("2d");
+          ctx1.lineWidth = 3;
+          ctx1.beginPath()
+          ctx1.moveTo(200, 110);
+          ctx1.lineTo(230, 140);
+          ctx1.stroke();
+  
+          for(i=0;i<numPalabras;i++){
+              document.getElementById('d'+i).value=arrayPalabras[i];
+          }
+  
+          document.getElementById('resultado').innerHTML = 'Perdiste estas Ahorcado';
+          boton.disabled=true;
+  
       }
-
-      document.getElementById('resultado').innerHTML = 'Perdiste estas Ahorcado';
-      boton.disabled=true;
-      break;
-
   }
+  const ahorcado = dibujoAhorcado[muñeco]
+  dibujoAhorcado[muñeco]()
 }
